f73893a42dbd734e9722eb6f67edd080997bb02c
Ritesh Kumar <glbitm2025@gmail.com>
2025-11-12 23:23:50 +0530

    Time: 0 ms (100.00%) | Memory: 25.9 MB (93.32%) - LeetSync

--- 838-design-linked-list/design-linked-list.cpp
+++ 838-design-linked-list/design-linked-list.cpp
@@ -1,93 +1,72 @@
 class MyLinkedList {
-private:
+public:
     struct Node {
         int val;
         Node* next;
-        Node(int v) : val(v), next(nullptr) {}
+        Node(int x) : val(x), next(nullptr) {}
     };
 
     Node* head;
     int size;
 
-public:
-    // Initialize your data structure here
     MyLinkedList() {
         head = nullptr;
         size = 0;
     }
-
-    // Get the value of the index-th node in the linked list. If invalid, return -1.
+    
     int get(int index) {
         if (index < 0 || index >= size) return -1;
         Node* curr = head;
-        for (int i = 0; i < index; i++) {
-            curr = curr->next;
-        }
+        for (int i = 0; i < index; i++) curr = curr->next;
         return curr->val;
     }
-
-    // Add a node of value val before the first element of the linked list
+    
     void addAtHead(int val) {
-        Node* newNode = new Node(val);
-        newNode->next = head;
-        head = newNode;
+        Node* temp = new Node(val);
+        temp->next = head;
+        head = temp;
         size++;
     }
-
-    // Append a node of value val to the last element of the linked list
+    
     void addAtTail(int val) {
-        Node* newNode = new Node(val);
+        Node* temp = new Node(val);
         if (!head) {
-            head = newNode;
+            head = temp;
         } else {
             Node* curr = head;
-            while (curr->next) {
-                curr = curr->next;
-            }
-            curr->next = newNode;
+            while (curr->next) curr = curr->next;
+            curr->next = temp;
         }
         size++;
     }
-
-    // Add a node of value val before the index-th node
+    
     void addAtIndex(int index, int val) {
         if (index < 0 || index > size) return;
-        if (index == 0) {
-            addAtHead(val);
-            return;
-        }
-        if (index == size) {
-            addAtTail(val);
-            return;
-        }
+        if (index == 0) { addAtHead(val); return; }
+        if (index == size) { addAtTail(val); return; }
 
         Node* curr = head;
-        for (int i = 0; i < index - 1; i++) {
-            curr = curr->next;
-        }
-        Node* newNode = new Node(val);
-        newNode->next = curr->next;
-        curr->next = newNode;
+        for (int i = 0; i < index - 1; i++) curr = curr->next;
+        Node* temp = new Node(val);
+        temp->next = curr->next;
+        curr->next = temp;
         size++;
     }
-
-    // Delete the index-th node in the linked list
+    
     void deleteAtIndex(int index) {
-        if (index < 0 || index >= size) return;
+        if (index < 0 || index >= size || head == nullptr) return;
 
+        Node* temp;
         if (index == 0) {
-            Node* temp = head;
+            temp = head;
             head = head->next;
-            delete temp;
         } else {
             Node* curr = head;
-            for (int i = 0; i < index - 1; i++) {
-                curr = curr->next;
-            }
-            Node* temp = curr->next;
-            curr->next = temp->next;
-            delete temp;
+            for (int i = 0; i < index - 1; i++) curr = curr->next;
+            temp = curr->next;
+            curr->next = curr->next->next;
         }
+        delete temp;
         size--;
     }
 };
