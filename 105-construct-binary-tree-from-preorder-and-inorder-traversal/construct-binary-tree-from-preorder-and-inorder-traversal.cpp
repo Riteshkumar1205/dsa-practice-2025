bfbfa948580dadf8dd446a6111a3628b3ef1d5d1
Ritesh Kumar <glbitm2025@gmail.com>
2025-11-27 23:42:48 +0530

    Time: 0 ms (100.00%) | Memory: 27.1 MB (79.73%) - LeetSync

--- 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
+++ 105-construct-binary-tree-from-preorder-and-inorder-traversal/construct-binary-tree-from-preorder-and-inorder-traversal.cpp
@@ -8,7 +8,7 @@
  *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
  *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
  * };
- */
+ 
 class Solution {
 public:
     TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
@@ -39,6 +39,45 @@ private:
 
         root->left = build(preorder, inorder, preIndex, inStart, inRoot - 1, inMap);
         root->right = build(preorder, inorder, preIndex, inRoot + 1, inEnd, inMap);
+
+        return root;
+    }
+};
+*/
+class Solution {
+public:
+    TreeNode* buildTree(vector<int>& preorder, vector<int>& inorder) {
+        if (preorder.empty()) return nullptr;
+
+        TreeNode* root = new TreeNode(preorder[0]);
+        stack<TreeNode*> st;
+        st.push(root);
+
+        int inIdx = 0;  
+
+        for (int i = 1; i < preorder.size(); i++) {
+
+            int val = preorder[i];
+            TreeNode* node = new TreeNode(val);
+
+            TreeNode* top = st.top();
+
+           
+            if (top->val != inorder[inIdx]) {
+                top->left = node;
+                st.push(node);
+            }
+
+            else {
+                while (!st.empty() && st.top()->val == inorder[inIdx]) {
+                    top = st.top();
+                    st.pop();
+                    inIdx++;
+                }
+                top->right = node;
+                st.push(node);
+            }
+        }
 
         return root;
     }
