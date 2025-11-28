a778d09d13aba21b6e9f4a242a09b77b977831a6
Ritesh Kumar <glbitm2025@gmail.com>
2025-11-28 23:35:35 +0530

    Add RemoveConsecutiveDuplicate.cpp

---
+++ RemoveConsecutiveDuplicate.cpp
@@ -0,0 +1,27 @@
+#include<iostream>
+#include<stack>
+#include<algorithm>
+using namespace std;
+
+string removeDuplicates(string s) {
+    stack<char> st;
+    st.push(s[0]);
+    for(int i = 1; i< s.length(); i++){
+        if(s[i] != st.top()) st.push(s[i]);
+    }
+    s = "";
+    while(!st.empty()) {
+        s += st.top();
+        st.pop();
+    }
+     reverse(s.begin(), s.end()); 
+    return s;
+}
+
+int main() {
+    string s = "aabbccdee";
+    cout << removeDuplicates(s) << endl;
+    s = removeDuplicates(s);
+    cout << s << endl;
+    return 0;
+}
