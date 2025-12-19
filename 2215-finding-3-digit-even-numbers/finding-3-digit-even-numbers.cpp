b9e9a16f0de21fbb769e212dbd0b9e077de1d636
Ritesh Kumar <glbitm2025@gmail.com>
2025-12-20 00:33:48 +0530

    Time: 4 ms (63.22%) | Memory: 12.5 MB (88.57%) - LeetSync

--- 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
+++ 2215-finding-3-digit-even-numbers/finding-3-digit-even-numbers.cpp
@@ -1,34 +1,33 @@
 class Solution {
 public:
     vector<int> findEvenNumbers(vector<int>& digits) {
-        vector<int> freq(10, 0);
-        for (int d : digits) freq[d]++;
-
         vector<int> ans;
+        unordered_map<int,int> m;
 
-        for (int num = 100; num <= 999; num += 2) {
-            int x = num;
-            int a = x % 10;        
-            x /= 10;
-            int b = x % 10;        
-            int c = x / 10;        
-
-            vector<int> need(10, 0);
-            need[a]++;
-            need[b]++;
-            need[c]++;
-
-            bool ok = true;
-            for (int d = 0; d <= 9; d++) {
-                if (need[d] > freq[d]) {
-                    ok = false;
-                    break;
-                }
-            }
-
-            if (ok) ans.push_back(num);
+        for (int ele : digits) {
+            m[ele]++;
         }
 
+        for (int i = 100; i <= 999; i += 2) {
+            int x = i;
+            int a = x % 10;
+            x /= 10;
+            int b = x % 10;
+            x /= 10;
+            int c = x;
+
+            if (m[a] > 0) {
+                m[a]--;
+                if (m[b] > 0) {
+                    m[b]--;
+                    if (m[c] > 0) {
+                        ans.push_back(i);
+                    }
+                    m[b]++;
+                }
+                m[a]++;
+            }
+        }
         return ans;
     }
 };
