285320277fa94441db08580aa992ba33547f8337
Ritesh Kumar <glbitm2025@gmail.com>
2026-01-07 18:38:18 +0530

    Clarify code with comment on splitting logic
    Added a comment to clarify the purpose of the code.

---
+++ 1339. Maximum Product of Splitted Binary Tree.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    long long totalSum = 0;
+    long long best = 0;
+    const int MOD = 1e9 + 7;
+
+    long long getTotalSum(TreeNode* root) {
+        if (!root) return 0;
+        return root->val + getTotalSum(root->left) + getTotalSum(root->right);
+    }
+
+    long long dfs(TreeNode* root) {
+        if (!root) return 0;
+
+        long long left = dfs(root->left);
+        long long right = dfs(root->right);
+
+        long long subTreeSum = root->val + left + right;
+
+
+        best = max(best, subTreeSum * (totalSum - subTreeSum));
+
+        return subTreeSum;
+    }
+
+    int maxProduct(TreeNode* root) {
+        totalSum = getTotalSum(root);
+        dfs(root);
+        return best % MOD;
+    }
+};
