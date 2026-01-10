cb8f441c077b11bb72ccd34c5c36dac7db1f3d98
Ritesh Kumar <glbitm2025@gmail.com>
2026-01-10 22:43:15 +0530

    Time: 2 ms (99.81%) | Memory: 9.5 MB (96.52%) - LeetSync

--- 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
+++ 712-minimum-ascii-delete-sum-for-two-strings/minimum-ascii-delete-sum-for-two-strings.cpp
@@ -3,23 +3,25 @@ public:
     int minimumDeleteSum(string s1, string s2) {
         int n = s1.size(), m = s2.size();
         
+        vector<int> dp(m + 1, 0);
 
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));
-        
         for (int i = 1; i <= n; i++) {
+            int prev = 0; 
             for (int j = 1; j <= m; j++) {
+                int temp = dp[j]; 
                 if (s1[i - 1] == s2[j - 1]) {
-                    dp[i][j] = dp[i - 1][j - 1] + s1[i - 1];
+                    dp[j] = prev + s1[i - 1];
                 } else {
-                    dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
+                    dp[j] = max(dp[j], dp[j - 1]);
                 }
+                prev = temp;
             }
         }
-        
+
         int sum1 = 0, sum2 = 0;
         for (char c : s1) sum1 += c;
         for (char c : s2) sum2 += c;
-        
-        return sum1 + sum2 - 2 * dp[n][m];
+
+        return sum1 + sum2 - 2 * dp[m];
     }
 };
