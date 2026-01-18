9057c863ec41777f760263af4f683dc1db644ddf
Ritesh Kumar <glbitm2025@gmail.com>
2026-01-18 22:18:11 +0530

    Time: 4 ms (90.41%) | Memory: 12.9 MB (72.67%) - LeetSync

--- 1311-largest-magic-square/largest-magic-square.cpp
+++ 1311-largest-magic-square/largest-magic-square.cpp
@@ -1,50 +1,42 @@
 class Solution {
 public:
-    int largestMagicSquare(vector<vector<int>>& grid) {
-        int m = grid.size(), n = grid[0].size();
+    int largestMagicSquare(vector<vector<int>>& g) {
+        int m = g.size(), n = g[0].size();
+        int ans = 1;
 
-        vector<vector<int>> row(m, vector<int>(n + 1, 0));
-        vector<vector<int>> col(m + 1, vector<int>(n, 0));
-        vector<vector<int>> d1(m + 1, vector<int>(n + 1, 0));
-        vector<vector<int>> d2(m + 1, vector<int>(n + 1, 0));
+        static int row[51][52], col[52][51], d1[52][52], d2[52][52];
 
         for (int i = 0; i < m; i++) {
             for (int j = 0; j < n; j++) {
-                row[i][j + 1] = row[i][j] + grid[i][j];
-                col[i + 1][j] = col[i][j] + grid[i][j];
-                d1[i + 1][j + 1] = d1[i][j] + grid[i][j];
-                d2[i + 1][j] = d2[i][j + 1] + grid[i][j];
+                row[i][j + 1] = row[i][j] + g[i][j];
+                col[i + 1][j] = col[i][j] + g[i][j];
+                d1[i + 1][j + 1] = d1[i][j] + g[i][j];
+                d2[i + 1][j] = d2[i][j + 1] + g[i][j];
             }
         }
 
-        int maxK = min(m, n);
-
-        for (int k = maxK; k >= 2; k--) {
+        for (int k = 2; k <= min(m, n); k++) {
             for (int i = 0; i + k <= m; i++) {
                 for (int j = 0; j + k <= n; j++) {
 
-                    int target = row[i][j + k] - row[i][j];
+                    int sum = row[i][j + k] - row[i][j];
 
-                    if (d1[i + k][j + k] - d1[i][j] != target) continue;
-                    if (d2[i + k][j] - d2[i][j + k] != target) continue;
+                    if (d1[i + k][j + k] - d1[i][j] != sum) continue;
+                    if (d2[i + k][j] - d2[i][j + k] != sum) continue;
 
                     bool ok = true;
 
-                    for (int r = i + 1; r < i + k && ok; r++) {
-                        if (row[r][j + k] - row[r][j] != target)
+                    for (int t = 0; t < k && ok; t++) {
+                        if (row[i + t][j + k] - row[i + t][j] != sum ||
+                            col[i + k][j + t] - col[i][j + t] != sum) {
                             ok = false;
+                        }
                     }
 
-                    
-                    for (int c = j; c < j + k && ok; c++) {
-                        if (col[i + k][c] - col[i][c] != target)
-                            ok = false;
-                    }
-
-                    if (ok) return k;
+                    if (ok) ans = k;
                 }
             }
         }
-        return 1;
+        return ans;
     }
 };
