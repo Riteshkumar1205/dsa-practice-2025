17dde6a08ff87565723ad7d9cbf34ef5785c5388
Ritesh Kumar <glbitm2025@gmail.com>
2026-01-23 22:03:29 +0530

    Time: 455 ms (93.04%) | Memory: 163.1 MB (91.14%) - LeetSync

--- 3772-minimum-pair-removal-to-sort-array-ii/minimum-pair-removal-to-sort-array-ii.cpp
+++ 3772-minimum-pair-removal-to-sort-array-ii/minimum-pair-removal-to-sort-array-ii.cpp
@@ -32,24 +32,19 @@ public:
             int li = left[i];      
             int rj = right[j];  
 
-            // Remove bad pairs that are disappearing
             if (li != -1 && a[i] < a[li]) bad--;
             if (rj != -1 && a[rj] < a[j]) bad--;
             if (a[j] < a[i]) bad--;
 
-            // Merge the pair i + j
             a[i] += a[j];
             alive[j] = false;
 
-            // Update linked list pointers
             right[i] = rj;
             if (rj != -1) left[rj] = i;
 
-            // Add new bad pairs after merge
             if (li != -1 && a[i] < a[li]) bad++;
             if (rj != -1 && a[rj] < a[i]) bad++;
 
-            // Push updated neighbor pairs to heap
             if (li != -1) pq.push({a[li] + a[i], li});
             if (rj != -1) pq.push({a[i] + a[rj], i});
 
