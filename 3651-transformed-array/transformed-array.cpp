959bebd299c06771d3dd814303e6f56e71f9857c
Ritesh Kumar <glbitm2025@gmail.com>
2026-02-05 22:02:00 +0530

    Time: 11 ms (18.11%) | Memory: 25.7 MB (70.75%) - LeetSync

--- 3651-transformed-array/transformed-array.cpp
+++ 3651-transformed-array/transformed-array.cpp
@@ -5,12 +5,9 @@ public:
         vector<int> result(n);
 
         for (int i = 0; i < n; i++) {
-            if (nums[i] == 0) {
-                result[i] = 0;
-            } else {
-                int newIndex = ((i + nums[i]) % n + n) % n;
-                result[i] = nums[newIndex];
-            }
+            int newIndex = (i + nums[i]) % n;
+            if (newIndex < 0) newIndex += n;
+            result[i] = nums[newIndex];
         }
 
         return result;
