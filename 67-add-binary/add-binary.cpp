526686a02ba53171874b1e4854228c89cac3d750
Ritesh Kumar <glbitm2025@gmail.com>
2026-02-15 22:05:23 +0530

    Time: 7 ms (4.52%) | Memory: 12.7 MB (5.65%) - LeetSync

--- 67-add-binary/add-binary.cpp
+++ 67-add-binary/add-binary.cpp
@@ -1,21 +1,34 @@
+#include <stack>
 class Solution {
 public:
     string addBinary(string a, string b) {
-        string result = "";
-        int i = a.size()-1;
-        int j = b.size()-1;
+        stack<int> s1, s2;
+        
+        // Push digits into stacks
+        for(char c : a) s1.push(c - '0');
+        for(char c : b) s2.push(c - '0');
+        
         int carry = 0;
-
-        while(i >= 0 || j >= 0 || carry) {
+        string result = "";
+        
+        // Process stacks
+        while(!s1.empty() || !s2.empty() || carry) {
             int sum = carry;
-
-            if(i >= 0) sum += a[i--] -'0';
-            if(j >= 0) sum += b[j--] -'0';
             
-            result += (sum % 2) + '0';
+            if(!s1.empty()) {
+                sum += s1.top();
+                s1.pop();
+            }
+            
+            if(!s2.empty()) {
+                sum += s2.top();
+                s2.pop();
+            }
+            
+            result = char((sum % 2) + '0') + result;
             carry = sum / 2;
         }
-        reverse(result.begin(), result.end());
+        
         return result;
     }
 };
