41efaa7c1cbc4e4d7af829b294a2994e73e81327
Ritesh Kumar <glbitm2025@gmail.com>
2026-02-17 20:15:07 +0530

    Time: 2 ms (22.13%) | Memory: 10.4 MB (11.98%) - LeetSync

---
+++ 401-binary-watch/binary-watch.cpp
@@ -0,0 +1,37 @@
+class Solution {
+public:
+    vector<string> result;
+    
+    void backtrack(int turnedOn, int index, int hour, int minute) {
+        // If LEDs used exceed limits, stop
+        if (hour > 11 || minute > 59) return;
+        
+        // If no LEDs left to turn on, record valid time
+        if (turnedOn == 0) {
+            string time = to_string(hour) + ":";
+            if (minute < 10) time += "0";
+            time += to_string(minute);
+            result.push_back(time);
+            return;
+        }
+        
+        // If we've checked all 10 LEDs
+        if (index == 10) return;
+        
+        // Choose current LED
+        if (index < 4) {
+            // First 4 LEDs are hours
+            backtrack(turnedOn - 1, index + 1, hour + (1 << index), minute);
+        } else {
+            // Last 6 LEDs are minutes
+            backtrack(turnedOn - 1, index + 1, hour, minute + (1 << (index - 4)));
+        }       
+        // Skip current LED
+        backtrack(turnedOn, index + 1, hour, minute);
+    }
+    
+    vector<string> readBinaryWatch(int turnedOn) {
+        backtrack(turnedOn, 0, 0, 0);
+        return result;
+    }
+};
