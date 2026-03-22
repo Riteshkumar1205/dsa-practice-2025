a3b2db5e958187ab7fd4c7880b44c6a97bcd2066
Ritesh Kumar <glbitm2025@gmail.com>
2026-03-22 22:58:17 +0530

    Time: 0 ms (100.00%) | Memory: 14.6 MB (70.77%) - LeetSync

--- 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
+++ 2015-determine-whether-matrix-can-be-obtained-by-rotation/determine-whether-matrix-can-be-obtained-by-rotation.cpp
@@ -1,3 +1,4 @@
+/*
 class Solution {
 public:
     void rotate(vector<vector<int>>& mat) {
@@ -21,4 +22,27 @@ public:
         }
         return false;
     }
+};
+*/
+class Solution {
+    public:
+          void rotate(vector<vector<int>>& mat){
+            int n = mat.size();
+
+            for(int i=0;i<n;i++){
+                for(int j=i+1;j<n;j++){
+                    swap(mat[i][j], mat[j][i]);
+                }
+            }
+            for(int i=0;i<n;i++){
+                reverse(mat[i].begin(),mat[i].end());
+            }
+          }
+          bool findRotation(vector<vector<int>>& mat, vector<vector<int>>& target){
+            for(int k=0;k<4;k++){
+                if(mat == target) return true;
+                rotate(mat);
+            }
+            return false;
+          }
 };
