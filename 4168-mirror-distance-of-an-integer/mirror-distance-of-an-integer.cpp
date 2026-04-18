7bb2a8c36598092636c4dd225e41dc3fce4d34f7
Ritesh Kumar <glbitm2025@gmail.com>
2026-04-18 23:23:04 +0530

    Time: 0 ms (100.00%) | Memory: 8.6 MB (60.81%) - LeetSync

---
+++ 4168-mirror-distance-of-an-integer/mirror-distance-of-an-integer.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    int reverseNum(int x) {
+        int rev = 0;
+        while (x > 0) {
+            rev = rev * 10 + (x % 10);
+            x /= 10;
+        }
+        return rev;
+    }
+
+    int mirrorDistance(int n) {
+        int rev = reverseNum(n);
+        return abs(n - rev);
+    }
+};
