ae8c32d2e1f462132d3bb9700adf542412e1b94d
Ritesh Kumar <glbitm2025@gmail.com>
2026-06-07 22:02:21 +0530

    Time: 188 ms (66.95%) | Memory: 279.1 MB (59.54%) - LeetSync

---
+++ 2306-create-binary-tree-from-descriptions/create-binary-tree-from-descriptions.cpp
@@ -0,0 +1,53 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* createBinaryTree(vector<vector<int>>& descriptions) {
+        unordered_map<int, TreeNode*> nodeMap;
+        unordered_set<int> children;
+
+        for (const auto& desc : descriptions) {
+            int parentVal = desc[0];
+            int childVal = desc[1];
+            bool isLeft = desc[2];
+
+            // Create parent node if it doesn't exist
+            if (nodeMap.find(parentVal) == nodeMap.end()) {
+                nodeMap[parentVal] = new TreeNode(parentVal);
+            }
+            // Create child node if it doesn't exist
+            if (nodeMap.find(childVal) == nodeMap.end()) {
+                nodeMap[childVal] = new TreeNode(childVal);
+            }
+
+            // Link parent and child
+            if (isLeft) {
+                nodeMap[parentVal]->left = nodeMap[childVal];
+            } else {
+                nodeMap[parentVal]->right = nodeMap[childVal];
+            }
+
+            // Mark this node as a child
+            children.insert(childVal);
+        }
+
+        // The root is the node that is never a child
+        for (const auto& desc : descriptions) {
+            int parentVal = desc[0];
+            if (children.find(parentVal) == children.end()) {
+                return nodeMap[parentVal];
+            }
+        }
+
+        return nullptr;
+    }
+};
