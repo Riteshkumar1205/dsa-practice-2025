ba1b36303946838f7b0b3726b9762ca85d813dc8
Ritesh Kumar <glbitm2025@gmail.com>
2026-08-09 22:41:05 +0530

    Time: 4 ms (89.03%) | Memory: 11 MB (97.16%) - LeetSync

--- 1240-stone-game-ii/stone-game-ii.cpp
+++ 1240-stone-game-ii/stone-game-ii.cpp
@@ -1,47 +1,35 @@
-#include <vector>
-#include <numeric>
-#include <algorithm>
-#include <cstring>
-
-using namespace std;
-
 class Solution {
-private:
+    private:
     int memo[101][101];
     int suffixSum[101];
     int n;
 
-    int dp(int i, int M, const vector<int>& piles) {
-        if (i >= n) return 0;
-        
-        if (i + 2 * M >= n) {
+    int dp(int i, int M, const vector<int>& piles){
+        if(i >= n) return 0;
+
+        if(i + 2* M >= n){
             return suffixSum[i];
         }
-
-        if (memo[i][M] != -1) {
+        if(memo[i][M] != -1){
             return memo[i][M];
         }
-
         int maxStones = 0;
-        for (int X = 1; X <= 2 * M; ++X) {
-            int opponentStones = dp(i + X, max(M, X), piles);
+        for(int X = 1; X <= 2*M; X++){
+            int opponentStones = dp(i + X, max(M,X), piles);
             int currentStones = suffixSum[i] - opponentStones;
             maxStones = max(maxStones, currentStones);
         }
-
         return memo[i][M] = maxStones;
     }
-
-public:
-    int stoneGameII(vector<int>& piles) {
+    public:
+    int stoneGameII(vector<int>& piles){
         n = piles.size();
-        memset(memo, -1, sizeof(memo));
-        
-        suffixSum[n - 1] = piles[n - 1];
-        for (int i = n - 2; i >= 0; --i) {
-            suffixSum[i] = suffixSum[i + 1] + piles[i];
-        }
+        memset(memo,-1,sizeof(memo));
 
-        return dp(0, 1, piles);
+        suffixSum[n - 1] = piles[n - 1];
+        for(int i = n-2;i >= 0;i--){
+            suffixSum[i] = suffixSum[i+1] + piles[i];
+        }
+        return dp(0,1, piles);
     }
 };
