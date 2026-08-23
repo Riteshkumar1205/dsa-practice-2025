79b7d18943bac045684247852be21d63ffac87f7
Ritesh Kumar <glbitm2025@gmail.com>
2026-08-23 22:30:56 +0530

    Time: 3 ms (66.67%) | Memory: 14 MB (67.83%) - LeetSync

--- 2039-sum-game/sum-game.cpp
+++ 2039-sum-game/sum-game.cpp
@@ -1,25 +1,25 @@
 class Solution {
-public:
-    bool sumGame(string num) {
+    public:
+    bool sumGame(string num){
         int n = num.size();
         int sum_diff = 0;
         int q_diff = 0;
 
-        for (int i = 0; i < n / 2; ++i) {
-            if (num[i] == '?') {
+        for(int i=0;i < n/2; i++){
+            if(num[i] == '?'){
                 q_diff++;
             } else {
                 sum_diff += (num[i] - '0');
             }
         }
-
-        for (int i = n / 2; i < n; ++i) {
-            if (num[i] == '?') {
+        for(int i=n/2; i < n; i++){
+            if(num[i] == '?') {
                 q_diff--;
-            } else {
+            }
+            else {
                 sum_diff -= (num[i] - '0');
             }
         }
-        return (sum_diff * 2 != -q_diff * 9);
+        return (sum_diff * 2 != -q_diff *9);
     }
 };
